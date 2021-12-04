3a54f4b2aef3fe7704d379c0188dab4aeb9d83fe
Fabian Fichter <ianfab@users.noreply.github.com>
2021-12-04 17:37:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# FairyFishGUI
+
+Minimalistic generic chess variant GUI using [pyffish](https://pypi.org/project/pyffish/) and [PySimpleGUI](https://github.com/PySimpleGUI/PySimpleGUI), based on the [PySimpleGUI Chess Demo](https://github.com/PySimpleGUI/PySimpleGUI/tree/master/Chess). Supports all chess variants supported by [Fairy-Stockfish](https://github.com/ianfab/Fairy-Stockfish)/pyffish.
+
+For well-known variants better use more polished GUIs like [LiGround](https://github.com/ml-research/liground). This project is meant as a fallback for variants where no other compatible GUI is available.
+
+## Installation
+
+Install dependencies (e.g., in a virtualenv) using pip
+
+    pip3 install -r requirements.txt
+
+## Run
+
+To start the GUI, run
+
+    python3 fairyfishgui.py
---
+++ fairyfishgui.py
@@ -0,0 +1,205 @@
+from contextlib import closing
+
+import PySimpleGUI as sg
+import pyffish
+
+MAX_FILES = 12
+MAX_RANKS = 10
+SQUARE_COLORS = ('#F0D9B5', '#B58863')
+PIECE_COLORS = ('white', 'black')
+
+
+class GameState():
+    def __init__(self, variant="chess", start_fen=None, moves=None):
+        self.variant = variant
+        self.start_fen = start_fen if start_fen else pyffish.start_fen(variant)
+        self.moves = moves if moves else []
+
+    def fen(self):
+        return pyffish.get_fen(self.variant, self.start_fen, self.moves)
+
+    def legal_moves(self):
+        return pyffish.legal_moves(self.variant, self.start_fen, self.moves)
+
+    def is_game_over(self):
+        return not self.legal_moves()
+
+    def is_legal(self, move):
+        return move in self.legal_moves()
+
+    def filter_legal(self, move):
+        return [m for m in self.legal_moves() if m.startswith(move) or m.endswith(move)]
+
+    def to_san(self, move=None):
+        if move:
+            return pyffish.get_san(self.variant, self.fen(), move)
+        return pyffish.get_san_moves(self.variant, self.start_fen, self.moves)
+
+    def push(self, move):
+        self.moves.append(move)
+
+    def pop(self):
+        if self.moves:
+            return self.moves.pop()
+
+    def files(self):
+        count = 0
+        last_char = ''
+        for c in pyffish.start_fen(self.variant).split('/')[0]:
+            if c.isdigit():
+                count += int(c)
+                if last_char.isdigit():
+                    count += 9 * int(last_char)
+            elif c.isalpha():
+                count += 1
+            last_char = c
+        return count
+
+    def ranks(self):
+        return pyffish.start_fen(self.variant).count('/') + 1
+
+    def char_board(self):
+        board = []
+        rank = []
+        prefix = ''
+        lastchar = ''
+        for c in self.fen():
+            if c == ' ':
+                board.append(rank)
+                break
+            elif c == '/':
+                board.append(rank)
+                rank = []
+            elif c.isdigit():
+                rank += int(c) * [' ']
+                if lastchar.isdigit():
+                    rank += 9 * int(lastchar) * [' ']
+            elif c == '+':
+                prefix = c
+            else:
+                rank.append(prefix + c)
+                prefix = ''
+            lastchar = c
+        return board
+
+
+class Board():
+    def __init__(self, *args):
+        self.state = GameState(*args)
+
+    @staticmethod
+    def to_file(file):
+        return chr(ord('a') + file)
+
+    def to_square(self, index):
+        return '{}{}'.format(self.to_file(index[1]), self.state.ranks() - index[0])
+
+    @staticmethod
+    def render_square(key, location):
+        square_color = SQUARE_COLORS[(location[0] + location[1]) % 2]
+        button = sg.Button(size=(3, 2), button_color=(PIECE_COLORS[0], square_color), pad=(0, 0), font='Any 20', key=key)
+        return sg.Column([[button]], pad=(0, 0))
+
+    def draw_board(self):
+        board_layout = []
+        for i in range(MAX_RANKS):
+            row = []
+            for j in range(MAX_FILES):
+                row.append(self.render_square(key=(i, j), location=(i, j)))
+            board_layout.append(row)
+        return board_layout
+
+    def update_board(self, window):
+        char_board = self.state.char_board()
+        for i in range(MAX_RANKS):
+            for j in range(MAX_FILES):
+                elem = window[(i, j)]
+                if i >= self.state.ranks() or j >= self.state.files():
+                    elem.update(visible=False)
+                else:
+                    square_color = SQUARE_COLORS[(i + j) % 2]
+                    piece = char_board[i][j]
+                    elem.update(visible=True, text=piece, button_color=(PIECE_COLORS[piece.islower()], square_color))
+        window['_movelist_'].update(' '.join(self.state.to_san()))
+
+
+class FairyGUI():
+    def __init__(self):
+        menu_def = [['&File', ['&Exit']], ['&Help', '&About...']]
+
+        sg.ChangeLookAndFeel('GreenTan')
+
+        board_controls = [[sg.Button('New Game', key='_newgame_'), sg.Button('Set FEN', key='_fen_')],
+                        [sg.Button('Move', key='_move_'), sg.Button('Undo', key='_undo_')],
+                        [sg.Text('Move List')],
+                        [sg.Multiline(do_not_clear=True, autoscroll=True, size=(15, 10), key='_movelist_')],
+                        ]
+
+        self.board = Board()
+        board_tab = [[sg.Column(self.board.draw_board())]]
+
+        layout = [[sg.Menu(menu_def, tearoff=False)],
+                [sg.TabGroup([[sg.Tab('Board', board_tab)]], title_color='red'),
+                sg.Column(board_controls)]]
+
+        self.window = sg.Window('FairyFishGUI',
+                        default_button_element_size=(12, 1),
+                        auto_size_buttons=False,
+                        resizable=True).Layout(layout)
+
+    @staticmethod
+    def popup(element, header, data, **kwargs):
+        layout = [[element(data, key='entry', **kwargs)], [sg.Button('OK')]]
+        with closing(sg.Window(header, layout).finalize()) as window:
+            while True:
+                event, values = window.read()
+                if event == sg.WINDOW_CLOSED or event == 'OK':
+                    if values and values['entry']:
+                        return values['entry']
+                    return
+
+    def run(self):
+        self.window.finalize()
+        while True:
+            self.board.update_board(self.window)
+            move_from = None
+            while True:
+                button, value = self.window.Read()
+                if button in (None, 'Exit'):
+                    exit()
+                elif button == '_newgame_':
+                    variant = self.popup(sg.Listbox, 'Variant', pyffish.variants(), size=(30, 20))
+                    if variant:
+                        self.board.state = GameState(variant[0])
+                        break
+                elif button == '_fen_':
+                    fen = self.popup(sg.Input, 'FEN', '', size=(30, 20))
+                    if fen:
+                        self.board.state = GameState(self.board.state.variant, fen)
+                        break
+                elif button == '_undo_':
+                    self.board.state.pop()
+                    self.board.update_board(self.window)
+                elif type(button) is tuple or button == '_move_':
+                    if move_from or button == '_move_':
+                        move = ''
+                        if move_from:
+                            move += self.board.to_square(move_from)
+                        if type(button) is tuple:
+                            move += self.board.to_square(button)
+                        moves = self.board.state.filter_legal(move)
+                        if len(moves) > 0:
+                            if len(moves) > 1:
+                                moves = self.popup(sg.Listbox, 'Choose move', moves, size=(20, 10))
+                            if moves:
+                                move = moves[0]
+                                self.board.state.push(move)
+                        move_from = None
+                        self.board.update_board(self.window)
+                    else:
+                        move_from = button
+                        self.window[move_from].update(button_color=('white', 'red'))
+
+
+if __name__ == '__main__':
+    FairyGUI().run()
